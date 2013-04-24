35380623c82026c15470c0452af34bedbca84004
Travis Lehman <travlehman@gmail.com>
2013-04-24 19:42:22 -0400

    stuff

---
+++ index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Bootstrap</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="/lib/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+	<link rel="shortcut icon" href="assets/ico/favicon.png">
+	
+	<!--typekit-->
+	<script type="text/javascript" src="//use.typekit.net/jbb4bbq.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+	
+	
+</head>
+
+<body data-spy="scroll" data-target=".bs-docs-sidebar">
+
+	<section id="intro">
+		<div class="container">
+			<h1><img src="/lib/img/logo-large.png" alt=""/></h1>
+			<p>Project Opus is a small team of filmmakers with large ideas&mdash;we love to work with great teams to create ideas that change the way people think.</p>
+		</div>
+	</section>
+	<div id="intro-nav">
+		<nav>
+			<a class="selected" href="">Home</a>
+			<a href="">What we do</a>
+			<a href="">Our world</a>
+			<a href="">Our work</a>
+			<a href="">Contact</a>
+		</nav>	
+	</div>
+	
+	<section id="reel">
+		<div class="videoWrapper">
+			<iframe src="http://player.vimeo.com/video/64176095?title=0&amp;byline=0&amp;portrait=0" width="100%" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+		
+		</div>
+	
+	</section><!--reel-->
+	
+	
+	<section id="whatwedo">
+		<div class="container">
+	
+			<header>
+				<div class="row-fluid">
+					<div class="span4">
+						<h2>What we do</h2>
+					</div>
+					<div class="span8">
+						<nav>
+							<a href="">Home</a>
+							<a class="selected" href="">What we do</a>
+							<a href="">Our world</a>
+							<a href="">Our work</a>
+							<a href="">Contact</a>
+						</nav>
+					</div>
+				</div>
+			</header>
+			
+			<h3>Our process</h3>
+		</div>
+		
+		<div id="process-gauge">
+			<strong class="start">Start</strong>
+			<strong class="finish">Finish</strong>
+			
+			<div class="container">
+				<div class="col one">
+					<span></span>
+				</div>
+				<div class="col two">
+					<span></span>
+				</div>
+				<div class="col three">
+					<span></span>
+				</div>
+			</div>
+		</div>
+			
+		<div class="container">
+			<div class="row-fluid">
+				<div class="span4">
+					<h4>Pre-production</h4>
+					<p>Start with a plan. This is where we map out the project, write the script, cast actors, etc. ProjectOpus’s producers and directors will help you map out a plan.</p> 
+					
+					<p>Pre-production is a vital role to the process. Our team will use this time to develop a strategy that will keep your project on-time and affordable.</p> 
+					
+					<p>Storyboarding, Scripting, Casting, Scheduling, location scouting, set design, logistics.</p> 
+					
+					<p>Let’s make something.</p>
+				</div>
+				
+				<div class="span4">
+					<h4>Production</h4>
+					<p>We make something every day. We know our equipment inside and out. At this point we might be setting up tripods on the beaches of Greece or filming the Archery world-cup in Tokyo, Japan. Or even shooting on a custom set built by master carpenters at our studio in Lancaster, PA. </p>
+					<p>We’ve done the planning during the pre-production process. Now is when we execute it. </p>
+				</div>
+				
+				<div class="span4">
+					<h4>Production</h4>
+					<p>So, we’ve made it this far. Now we hand off the project to our post-production team who will edit, animate, add motion graphics, compositing, color corrections, record voice-overs, and color the final product.</p> 
+					<p>You tell us how you want it delivered: FTP? Snail Mail? Or we’ll upload it to the services of your choosing.</p> 
+				</div>
+			</div>
+			
+			
+			
+			<div id="timeline" class="row-fluid">
+				<div class="span4">
+					<p class="large-text">Now that you know what the 3-stages of video production are, 
+					let’s take a look at what the typical process might look like.</p>
+					
+					<p>There are a lot of steps to this process, but we’re here to
+					 help. We’re experienced with large projects with big moving parts. 
+					 ProjectOpus looks forward to serving you.</p>
+				</div>
+				
+				<div class="span8">
+					
+					<div class="infographic">
+						<ol>
+							<li class="step1 below"><span>Client vision meeting</span></li>
+							<li class="step2 above"><span>Develop ideas</span></li>
+							<li class="step3 below"><span>Storyboard</span></li>
+							<li class="step4 above"><span>Script</span></li>
+							<li class="step5 below"><span>Concept approval</span></li>
+							<li class="step6 above"><span>Book talent</span></li>
+							<li class="step7 below"><span>Book locations &amp; build set</span></li>
+							<li class="step8 above"><span>Schedule shoot</span></li>
+							<li class="step9 below"><span>Film</span></li>
+							<li class="step10 above"><span>Assistant editor makes a rough cut</span></li>
+							<li class="step11 below"><span>Senior editor makes revisions</span></li>
+							<li class="step12 above"><span>Motion graphics or compositing</span></li>
+							<li class="step13 below"><span>First client review</span></li>
+							<li class="step14 above"><span>Music</span></li>
+							<li class="step15 below"><span>Audio mixing</span></li>
+							<li class="step16 above"><span>Color correction</span></li>
+							<li class="step17 below"><span>Second client review</span></li>
+							<li class="step18 above"><span>Revisions &amp; refinements</span></li>
+							<li class="step19 below"><span>Client approval</span></li>
+							<li class="step20 above"><span>Delivery</span></li>
+						</ol>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+	
+	</section><!--whatwedo-->
+	
+    
+
+
+
+
+<!--
+<div class="container">
+
+  <div class="marketing">
+
+    <h1>Introducing Bootstrap.</h1>
+    <p class="marketing-byline">Need reasons to love Bootstrap? Look no further.</p>
+
+    <div class="row-fluid">
+      <div class="span4">
+        <img class="marketing-img" src="assets/img/bs-docs-twitter-github.png">
+        <h2>By nerds, for nerds.</h2>
+        <p>Built at Twitter by <a href="http://twitter.com/mdo">@mdo</a> and <a href="http://twitter.com/fat">@fat</a>, Bootstrap utilizes <a href="http://lesscss.org">LESS CSS</a>, is compiled via <a href="http://nodejs.org">Node</a>, and is managed through <a href="http://github.com">GitHub</a> to help nerds do awesome stuff on the web.</p>
+      </div>
+      <div class="span4">
+        <img class="marketing-img" src="assets/img/bs-docs-responsive-illustrations.png">
+        <h2>Made for everyone.</h2>
+        <p>Bootstrap was made to not only look and behave great in the latest desktop browsers (as well as IE7!), but in tablet and smartphone browsers via <a href="./scaffolding.html#responsive">responsive CSS</a> as well.</p>
+      </div>
+      <div class="span4">
+        <img class="marketing-img" src="assets/img/bs-docs-bootstrap-features.png">
+        <h2>Packed with features.</h2>
+        <p>A 12-column responsive <a href="./scaffolding.html#gridSystem">grid</a>, dozens of components, <a href="./javascript.html">JavaScript plugins</a>, typography, form controls, and even a <a href="./customize.html">web-based Customizer</a> to make Bootstrap your own.</p>
+      </div>
+    </div>
+
+    <hr class="soften">
+
+    <h1>Built with Bootstrap.</h1>
+    <p class="marketing-byline">For even more sites built with Bootstrap, <a href="http://builtwithbootstrap.tumblr.com/" target="_blank">visit the unofficial Tumblr</a> or <a href="./getting-started.html#examples">browse the examples</a>.</p>
+    <div class="row-fluid">
+      <ul class="thumbnails example-sites">
+        <li class="span3">
+          <a class="thumbnail" href="http://soundready.fm/" target="_blank">
+            <img src="assets/img/example-sites/soundready.png" alt="SoundReady.fm">
+          </a>
+        </li>
+        <li class="span3">
+          <a class="thumbnail" href="http://kippt.com/" target="_blank">
+            <img src="assets/img/example-sites/kippt.png" alt="Kippt">
+          </a>
+        </li>
+        <li class="span3">
+          <a class="thumbnail" href="http://www.gathercontent.com/" target="_blank">
+            <img src="assets/img/example-sites/gathercontent.png" alt="Gather Content">
+          </a>
+        </li>
+        <li class="span3">
+          <a class="thumbnail" href="http://www.jshint.com/" target="_blank">
+            <img src="assets/img/example-sites/jshint.png" alt="JS Hint">
+          </a>
+        </li>
+      </ul>
+     </div>
+
+  </div>
+
+</div>
+
+
+
+    <footer class="footer">
+      <div class="container">
+        <p>Designed and built with all the love in the world by <a href="http://twitter.com/mdo" target="_blank">@mdo</a> and <a href="http://twitter.com/fat" target="_blank">@fat</a>.</p>
+        <p>Code licensed under <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">Apache License v2.0</a>, documentation under <a href="http://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>.</p>
+        <p><a href="http://glyphicons.com">Glyphicons Free</a> licensed under <a href="http://creativecommons.org/licenses/by/3.0/">CC BY 3.0</a>.</p>
+        <ul class="footer-links">
+          <li><a href="http://blog.getbootstrap.com">Blog</a></li>
+          <li class="muted">&middot;</li>
+          <li><a href="https://github.com/twitter/bootstrap/issues?state=open">Issues</a></li>
+          <li class="muted">&middot;</li>
+          <li><a href="https://github.com/twitter/bootstrap/blob/master/CHANGELOG.md">Changelog</a></li>
+        </ul>
+      </div>
+    </footer>
+
+
+
+    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap-transition.js"></script>
+    <script src="assets/js/bootstrap-alert.js"></script>
+    <script src="assets/js/bootstrap-modal.js"></script>
+    <script src="assets/js/bootstrap-dropdown.js"></script>
+    <script src="assets/js/bootstrap-scrollspy.js"></script>
+    <script src="assets/js/bootstrap-tab.js"></script>
+    <script src="assets/js/bootstrap-tooltip.js"></script>
+    <script src="assets/js/bootstrap-popover.js"></script>
+    <script src="assets/js/bootstrap-button.js"></script>
+    <script src="assets/js/bootstrap-collapse.js"></script>
+    <script src="assets/js/bootstrap-carousel.js"></script>
+    <script src="assets/js/bootstrap-typeahead.js"></script>
+    <script src="assets/js/bootstrap-affix.js"></script>
+
+    <script src="assets/js/holder/holder.js"></script>
+    <script src="assets/js/google-code-prettify/prettify.js"></script>
+
+    <script src="assets/js/application.js"></script>
+
+
+
+  </body>
+</html>
+-->
